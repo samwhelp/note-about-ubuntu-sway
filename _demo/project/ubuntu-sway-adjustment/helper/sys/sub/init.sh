997ca012f9e0bae0ba33c4e956f7e74479314de8
samwhelp <samwhelp@users.noreply.github.com>
2022-09-19 18:18:01 +0800

    fix

--- _demo/project/ubuntu-sway-adjustment/helper/sys/sub/init.sh
+++ _demo/project/ubuntu-sway-adjustment/helper/sys/sub/init.sh
@@ -17,13 +17,25 @@ source "${THE_SYS_SUB_DIR_PATH}/main.sh"
 ##
 ## sub module
 ##
+
+##
+## wm
+##
 source "${THE_SUB_DIR_PATH}/sway/ext/init.sh"
-source "${THE_SUB_DIR_PATH}/style/ext/init.sh"
+
+
+##
+## tool
+##
 source "${THE_SUB_DIR_PATH}/sakura/ext/init.sh"
 source "${THE_SUB_DIR_PATH}/pcmanfm-qt/ext/init.sh"
 source "${THE_SUB_DIR_PATH}/pcmanfm/ext/init.sh"
 source "${THE_SUB_DIR_PATH}/viewnior/ext/init.sh"
 source "${THE_SUB_DIR_PATH}/mpv/ext/init.sh"
+
+##
+## env
+##
 source "${THE_SUB_DIR_PATH}/fcitx5/ext/init.sh"
 source "${THE_SUB_DIR_PATH}/font/ext/init.sh"
 source "${THE_SUB_DIR_PATH}/rofi/ext/init.sh"
@@ -32,6 +44,13 @@ source "${THE_SUB_DIR_PATH}/xdg/ext/init.sh"
 source "${THE_SUB_DIR_PATH}/tool/ext/init.sh"
 source "${THE_SUB_DIR_PATH}/helper/ext/init.sh"
 source "${THE_SUB_DIR_PATH}/wallpaper/ext/init.sh"
+
+##
+## sudo
+##
+source "${THE_SUB_DIR_PATH}/apt/ext/init.sh"
+source "${THE_SUB_DIR_PATH}/locale/ext/init.sh"
+source "${THE_SUB_DIR_PATH}/style/ext/init.sh"
 
 ##
 ### Tail: Func / Init
--- _demo/project/ubuntu-sway-adjustment/helper/sys/sub/main.sh
+++ _demo/project/ubuntu-sway-adjustment/helper/sys/sub/main.sh
@@ -23,12 +23,23 @@ mod_main_package_install () {
 
 mod_main_package_list_find_for_install () {
 
+	##
+	## wm
+	##
 	mod_sway_package_list_find_for_install
+
+	##
+	## tool
+	##
 	mod_sakura_package_list_find_for_install
 	mod_pcmanfm_qt_package_list_find_for_install
 	mod_pcmanfm_package_list_find_for_install
 	mod_viewnior_package_list_find_for_install
 	mod_mpv_package_list_find_for_install
+
+	##
+	## env
+	##
 	mod_fcitx5_package_list_find_for_install
 	mod_font_package_list_find_for_install
 	mod_rofi_package_list_find_for_install
@@ -38,7 +49,11 @@ mod_main_package_list_find_for_install () {
 	mod_helper_package_list_find_for_install
 	mod_wallpaper_package_list_find_for_install
 
-
+	##
+	## sudo
+	##
+	mod_apt_package_list_find_for_install
+	mod_locale_package_list_find_for_install
 	mod_style_package_list_find_for_install
 
 }
@@ -63,12 +78,23 @@ mod_main_config_install () {
 	util_error_echo
 	util_error_echo
 
+	##
+	## wm
+	##
 	mod_sway_config_install
+
+	##
+	## tool
+	##
 	mod_sakura_config_install
 	mod_pcmanfm_qt_config_install
 	mod_pcmanfm_config_install
 	mod_viewnior_config_install
 	mod_mpv_config_install
+
+	##
+	## env
+	##
 	mod_fcitx5_config_install
 	mod_font_config_install
 	mod_rofi_config_install
@@ -78,7 +104,11 @@ mod_main_config_install () {
 	mod_helper_config_install
 	mod_wallpaper_config_install
 
-
+	##
+	## sudo
+	##
+	mod_apt_config_install
+	mod_locale_config_install
 	mod_style_config_install
 
 
@@ -105,13 +135,23 @@ mod_main_asset_install () {
 	util_error_echo '##'
 	util_error_echo
 
-
+	##
+	## wm
+	##
 	mod_sway_asset_install
+
+	##
+	## tool
+	##
 	mod_sakura_asset_install
 	mod_pcmanfm_qt_asset_install
 	mod_pcmanfm_asset_install
 	mod_viewnior_asset_install
 	mod_mpv_asset_install
+
+	##
+	## env
+	##
 	mod_fcitx5_asset_install
 	mod_font_asset_install
 	mod_rofi_asset_install
@@ -121,7 +161,11 @@ mod_main_asset_install () {
 	mod_helper_asset_install
 	mod_wallpaper_asset_install
 
-
+	##
+	## sudo
+	##
+	mod_apt_asset_install
+	mod_locale_asset_install
 	mod_style_asset_install
 
 	util_error_echo
